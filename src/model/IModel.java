00c7428288c5b1073a661513c1743c61b3c082cc
Tymon <33154848+tymonstuff@users.noreply.github.com>
2018-12-14 14:42:39 +0000

    Create interface for Model methods
    Added the getters and setters for the data derived from the model. The data is the balls a player has.

--- src/model/IModel.java
+++ src/model/IModel.java
@@ -2,5 +2,9 @@ package model;
 
 public interface IModel
 {
-
+	int getPlayerBalls(int playerNum, int holeNum);
+	int getPlayerScore(int playerNum);
+	
+	void setPlayerBalls(int playerNum, int hole, int ballCount);
+	void setPlayerScore(int playerNum, int score);
 }
